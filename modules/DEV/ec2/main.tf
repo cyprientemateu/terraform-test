c27c2e655d0883465ceaadf62673dc33a636c85d
cyprientemateu <cyprien.temateu@yahoo.com>
2024-03-13 20:50:37 -0500

    added new file

--- modules/DEV/ec2/main.tf
+++ modules/DEV/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "tcc_ec2" {
   ami                    = data.aws_ami.custom_ami.id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = [var.vpc_security_group_ids[0]]
+  vpc_security_group_ids = [aws_security_group.tcc_sg.id]
   subnet_id              = var.subnet_id
   root_block_device {
     volume_size = var.volume_size
